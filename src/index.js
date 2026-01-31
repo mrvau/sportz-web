f8e7c7deb15276c06e13ab850909eb0de9d897bc
mr_vau <mdalif1434@gmail.com>
2026-02-01 01:12:47 +0600

    implement matches GET and POST routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchesRouter } from "./routes/matches.js";
 
 const app = express();
 const PORT = 8000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to the Express server!" });
 });
+
+app.use("/matches", matchesRouter);
 
 // Start server
 app.listen(PORT, () => {
